8e77e3e4395bf671a6ede624ef8eb2c4ce8ab635
andrea gauvreau <andrea.gauvreau31@gmail.com>
2025-05-28 21:20:11 +0800

    get ready for install

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'], // adapte selon tes fichiers
+      manifest: {
+        name: 'SEO Checklist App',
+        short_name: 'SEO App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
